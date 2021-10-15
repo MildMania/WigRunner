08408c3f3a4c6fc26343ffb6eb4375dd090022ac
M. Cem Kobal <cem.kobal@mildmania.com>
2021-10-15 12:56:23 +0300

    Merge develop into FixLevelEnd

--- Assets/Scripts/CameraSystem/VirtualCameraWin.cs
+++ Assets/Scripts/CameraSystem/VirtualCameraWin.cs
@@ -1,3 +1,19 @@
+using UnityEngine;
+using DG.Tweening;
+
 public class VirtualCameraWin : VirtualCameraBase
 {
+    [SerializeField] private float _winCameraTravelDuration = 1f;
+
+    protected override void ActivateCustomActions()
+    {
+        var dollycart = transform.GetComponentInChildren<Cinemachine.CinemachineDollyCart>();
+
+        DOTween.To(() => dollycart.m_Position,
+                   p => dollycart.m_Position = p,
+                   1,
+                   _winCameraTravelDuration);
+
+        base.ActivateCustomActions();
+    }
 }
