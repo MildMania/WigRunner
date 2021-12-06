af3fcbd56c559f9d4575ab51e2eb3a4927acbb1d
aenesMM <alp.enes.arici@mildmania.com>
2021-12-06 14:03:45 +0300

    Win camera update

--- Assets/Scripts/CameraSystem/VirtualCameraWin.cs
+++ Assets/Scripts/CameraSystem/VirtualCameraWin.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class VirtualCameraWin : VirtualCameraBase
 {
-    [SerializeField] private float _winCameraTravelDuration = 1f;
+    //[SerializeField] private float _winCameraTravelDuration = 1f;
 
 
-    private CinemachineTrackedDolly _cinemachineTrackedDolly;
-    private CinemachineTrackedDolly _CinemachineTrackedDolly
-    {
-        get
-        {
-            if (_cinemachineTrackedDolly == null)
-                _cinemachineTrackedDolly = VirtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+    //private CinemachineTrackedDolly _cinemachineTrackedDolly;
+    //private CinemachineTrackedDolly _CinemachineTrackedDolly
+    //{
+    //    get
+    //    {
+    //        if (_cinemachineTrackedDolly == null)
+    //            _cinemachineTrackedDolly = VirtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
 
-            return _cinemachineTrackedDolly;
-        }
-    }
+    //        return _cinemachineTrackedDolly;
+    //    }
+    //}
 
-    protected override void ActivateCustomActions()
-    {
-        DOTween.To(() => _CinemachineTrackedDolly.m_PathPosition,
-                   p => _CinemachineTrackedDolly.m_PathPosition = p,
-                   _CinemachineTrackedDolly.m_Path.MaxPos,
-                   _winCameraTravelDuration);
+    //protected override void ActivateCustomActions()
+    //{
+    //    DOTween.To(() => _CinemachineTrackedDolly.m_PathPosition,
+    //               p => _CinemachineTrackedDolly.m_PathPosition = p,
+    //               _CinemachineTrackedDolly.m_Path.MaxPos,
+    //               _winCameraTravelDuration);
 
-        base.ActivateCustomActions();
-    }
+    //    base.ActivateCustomActions();
+    //}
 }
