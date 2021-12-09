df4b9caba0db1301540d11916278dd6cb1b4d6de
aenesMM <alp.enes.arici@mildmania.com>
2021-12-09 17:20:00 +0300

    "CU-1wzp91d - Ribbon fix

--- Assets/Scripts/GateScripts/ChangeHairTypeGate.cs
+++ Assets/Scripts/GateScripts/ChangeHairTypeGate.cs
@@ -2,9 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class HairIcon
+{
+    public HairType HairType;
+    public Material HairIconMaterial;
+}
+
 public class ChangeHairTypeGate : GateBase
 {
+    [SerializeField] private List<HairIcon> _hairIcons;
+    
     [SerializeField] private HairType _hairType;
+    [SerializeField] private Renderer _renderer;
+
+
+    
+    private Material GetHairIconMaterial(HairType hairType)
+    {
+        Material mat = _hairIcons[0].HairIconMaterial;
+
+        foreach (var item in _hairIcons)
+        {
+            if (item.HairType == _hairType)
+            {
+                mat = item.HairIconMaterial;
+                break;
+            }
+        }
+
+        return mat;
+    }
+
     public override void OnEnteredGate()
     {
         if (_isEntered)
@@ -16,5 +45,14 @@ public class ChangeHairTypeGate : GateBase
         Character.Instance.CharacterVisualController.SetHairModelActive(_hairType);
 
         _isEntered = true;
+    }
+
+    private void OnValidate()
+    {
+        var mats = _renderer.materials;
+
+        mats[2] = GetHairIconMaterial(_hairType);
+
+        _renderer.materials = mats;
     }
 }
